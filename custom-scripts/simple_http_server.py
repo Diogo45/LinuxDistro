2f2ffca0938732f12521881bcc4079c37e3785a8
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-08-27 09:34:52 -0300

    Added time, uptime and sytem info in the html server

--- custom-scripts/simple_http_server.py
+++ custom-scripts/simple_http_server.py
@@ -1,5 +1,7 @@
 import time
 import BaseHTTPServer
+from datetime import datetime
+import pytz
 
 
 HOST_NAME = '0.0.0.0'
@@ -16,11 +18,21 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
+        s.wfile.write("<html><head><title>Basic HTML Server.</title></head>")
+        s.wfile.write("<body><p>Exibiting basic system info.</p>")
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
-        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+
+	tz_BR = pytz.timezone('America/Sao_Paulo')
+	now = datetime.now(tz_BR)
+        s.wfile.write("<p>The time is: %s</p>" % now)
+
+	fileup = open("/proc/uptime","r")
+	uptime = fileup.read().split(' ')[0]
+	s.wfile.write("<p>Uptime: %s</p>" % uptime)
+
+	cpuinfo = 
+
         s.wfile.write("</body></html>")
 
 if __name__ == '__main__':
